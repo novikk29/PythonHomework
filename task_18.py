7a9aae9cc6e9e0df39e72ed1aa39028c771195ef
Andrei <novikk1986@ya.ru>
2023-03-26 11:47:32 +0300

    add tasks 22, 24

--- task_18.py
+++ task_18.py
@@ -12,21 +12,27 @@ from random import randint
 n = int(input("Введите количество элементов: "))
 x = int(input("Введите число X: "))
 print()
-list_1 = [randint(1, 100) for _ in range(n)]
+list_1 = [randint(1, 5) for _ in range(n)]
 print(list_1)
-print()
+# print()
 print('x =', x)
-print()
-list_1 = sorted(list_1)
-for i in list_1:
-    if i <= x: 
-        number = i
-    else:
-        number = i
-        break
-print('самый близкий по величине элемент массива к заданному числу равен', number)
-print()
-
+# print()
+# list_1 = sorted(list_1)
+# for i in list_1:
+#     if i <= x: 
+#         number = i
+#     else:
+#         number = i
+#         break
+# print('самый близкий по величине элемент массива к заданному числу равен', number)
+# print()
+min_diff = abs(x - list_1[0])
+index_min_diff = 0
+for num in range(1,len(list_1)):
+    if min_diff > abs(x-list_1[num]):
+        min_diff = abs(x-list_1[num])
+        index_min_diff = num
+print(f'На позиции {index_min_diff} в списке находится самый близкий к {x} элемент и он равен {list_1[index_min_diff]}')
     
 
     
