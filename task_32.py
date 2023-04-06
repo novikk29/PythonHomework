65f51e82db3cf2acde12adfc01791ffe756730bf
Andrei <novikk1986@ya.ru>
2023-04-06 15:47:35 +0300

    add tasks 34 and 36

--- task_32.py
+++ task_32.py
@@ -3,19 +3,24 @@
 # Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
 
+from random import randint
 import os
 os.system('clear')
-from random import randint
+
 
 def fillArray(arraySize):
     return [randint(1, 10) for _ in range(arraySize)]
 
-def searchIndex(array):
-    arr = []
-    for i in range(len(array_1)):
-        if min_num <= array[i] <= max_num:
-            arr.append(i)
-    return arr
+
+def searchIndex(array_1, min_num, max_num):
+    return [i for i, num in enumerate(array_1) if min_num <= num <= max_num]
+    # arr = []
+    # for i, num in enumerate(array_1):
+    #     if min_num <= num <= max_num:
+    #         arr.append(i)
+    # return arr
+
+
 array_1 = fillArray(int(input('введите количество элементов массива: ')))
 min_num = int(input('введите минимальное значение диапазона: '))
 max_num = int(input('введите максимальное значение диапазона: '))
